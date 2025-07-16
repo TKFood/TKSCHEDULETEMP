2d8d9375b92c28255c5fd593147e9bc233181353
at3158 <tk290@tkfood.com.tw>
2025-07-16 14:27:06 +0800

    ADD_TKPOSTEMP

--- TKSCHEDULETEMP/FrmSCHEDULE.cs
+++ TKSCHEDULETEMP/FrmSCHEDULE.cs
@@ -74,16 +74,85 @@ namespace TKSCHEDULETEMP
                 return null;
             }
         }
+        /// <summary>
+        /// 新增pos資料，到TKPOSTEMP
+        /// </summary>
+        /// <param name="DT"></param>
+        public void ADD_TKPOSTEMP(DataTable DT)
+        {
+            StringBuilder SQL = new StringBuilder();
+
+            foreach (DataRow DR in DT.Rows)
+            {
+                string IP = DR["IP"].ToString();
+                string DBNAME = DR["DBNAME"].ToString();
+
+                // 解密連線字串
+                Class1 TKID = new Class1();
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbTKSCHEDULETEMP"].ConnectionString);
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                using (SqlConnection conn = new SqlConnection(sqlsb.ConnectionString))
+                {
+                    SQL.AppendFormat(@"
+                                    SELECT *
+                                    FROM [192.168.1.131].[COSMOS_POS].dbo.POSTB WITH(NOLOCK)
+                                    WHERE TB001 + TB002 + TB003 + TB006 + TB007 NOT IN 
+                                    (
+                                        SELECT TB001 + TB002 + TB003 + TB006 + TB007 
+                                        FROM [TKPOSTEMP].[dbo].[POSTB]
+                                    )
+                                    AND TB001 >= '20250715'
+                                        ");
+
+                    using (SqlCommand cmd = new SqlCommand(SQL.ToString(), conn))
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        if (dt.Rows.Count > 0)
+                        {
+                            // Step 3: SqlBulkCopy 進行整批寫入
+                            using (SqlBulkCopy bulkCopy = new SqlBulkCopy(conn))
+                            {
+                                bulkCopy.DestinationTableName = "[TKPOSTEMP].[dbo].[POSTB]";
+                                bulkCopy.BatchSize = 1000; // 每次一千筆
+                                bulkCopy.BulkCopyTimeout = 60;
+
+                                // 自動對應欄位（欄位名稱相同會自動對應）
+                                foreach (DataColumn col in dt.Columns)
+                                {
+                                    bulkCopy.ColumnMappings.Add(col.ColumnName, col.ColumnName);
+                                }
+
+                                conn.Open();
+                                bulkCopy.WriteToServer(dt);
+                                conn.Close();
+
+                                //MessageBox.Show($"成功寫入 {dt.Rows.Count} 筆資料！");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("無需寫入，無新資料。");
+                        }
+                    }
+                }
+            }
+        }
+
         #endregion
 
         #region BUTTON
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DataTable dt = FIND_POSIP();
-            if (dt != null)
-            {
-                MessageBox.Show(dt.Rows.Count.ToString());
+            DataTable DT = FIND_POSIP();
+            if (DT != null)
+            {               
+                ADD_TKPOSTEMP(DT);
             }
             else
             {

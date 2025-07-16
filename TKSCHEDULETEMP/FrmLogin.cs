d4f636edaf5ec6d8caeda2e9613a20aa21e2712a
at3158 <tk290@tkfood.com.tw>
2025-07-16 09:47:47 +0800

    button1_Click

---
+++ TKSCHEDULETEMP/FrmLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection;
+using TKITDLL;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TKSCHEDULETEMP
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
